c184d77e299f3db151e26f8520d35eee7f012512
Michael Femia <michael.femia@gmail.com>
2015-06-11 11:18:02 -0400

    Emptied globalVariables.php

--- mgmt/globalVariables.php
+++ mgmt/globalVariables.php
@@ -1,18 +1,18 @@
 <?
-$html_root="http://www.michaelfemia.com/demoSite";
-$domain_title="Demo Site";
-$titleTagline="CMS";
-$adminName="Mike";
-$adminEmail="michael.femia@gmail.com";
-$adminPassword="scroton";
-$twitterHandle="scroton";
-$dbHost="DemoSiteCMS.db.9310418.hostedresource.com";
-$dbDatabase="DemoSiteCMS";
-$dbUsername="DemoSiteCMS";
-$dbPassword="Drilldo65!";
-$imageDirectory="img";
-$searchDescription="CMS Designed By Michael Femia";
-$socialMediaThumbIMG="hpstore.png";
-$searchKeywords="Mike Femia";
-$googleAnalytics="<script></script>";
+$html_root="";
+$domain_title="";
+$titleTagline="";
+$adminName="";
+$adminEmail="";
+$adminPassword="";
+$twitterHandle="";
+$dbHost="";
+$dbDatabase="";
+$dbUsername="";
+$dbPassword="";
+$imageDirectory="";
+$searchDescription="";
+$socialMediaThumbIMG="";
+$searchKeywords="";
+$googleAnalytics="";
 ?>
